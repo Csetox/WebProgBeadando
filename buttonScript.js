e6f85df49bb899993e6db44ffa2e08c64b2c1e57
Csetox <97190328+Csetox@users.noreply.github.com>
2024-04-18 23:57:30 +0200

    uploaded files

---
+++ buttonScript.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btnHome = document.getElementById('btnBemutatkozas');
+    btnHome.addEventListener('click', function() {
+        window.location.href = 'index.html'; // Change the URL to index.html
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const btnRoplabda = document.getElementById('btnRoplabda');
+    btnRoplabda.addEventListener('click', function() {
+        window.location.href = 'roplabda.html'; // Change the URL to roplabda.html
+    });
+});
